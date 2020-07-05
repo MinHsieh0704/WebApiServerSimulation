c24ed1815cf362c9eb842e04422d4bd811af28d9
Min.Hsieh <Min.Hsieh@isapsolution.com>
2020-07-05 12:38:24 +0800

    Add IndexController

---
+++ WebApiServerSimulation/Controllers/IndexController.cs
@@ -0,0 +1,77 @@
+﻿using Min_Helpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApiServerSimulation.Controllers
+{
+    public class IndexController : ApiController
+    {
+        [HttpGet, HttpPost, HttpPut, HttpDelete]
+        public IHttpActionResult Handle()
+        {
+            try
+            {
+                HttpRequestMessage req = Request;
+                Uri reqUri = req.RequestUri;
+                HttpMethod reqMethod = req.Method;
+                HttpRequestHeaders reqHeaders = req.Headers;
+                HttpContent reqContent = req.Content;
+
+                JObject _input = new JObject();
+                if (reqMethod == HttpMethod.Get || reqMethod == HttpMethod.Delete)
+                {
+                    NameValueCollection input = HttpUtility.ParseQueryString(reqUri.Query);
+
+                    foreach (var key in input)
+                        _input.Add(new JProperty(key.ToString(), input[key.ToString()]));
+                }
+                else if (reqMethod == HttpMethod.Post || reqMethod == HttpMethod.Put)
+                {
+                    string input = reqContent.ReadAsStringAsync().Result;
+
+                    _input = JsonConvert.DeserializeObject<JObject>(input);
+                }
+
+                string info = "";
+                info += $"\r\n{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}";
+                info += $"\r\n    {reqMethod.Method}, {reqUri.LocalPath}";
+                info += "\r\n    Headers";
+                foreach(var header in reqHeaders)
+                {
+                    if (header.Value == null) continue;
+                    info += $"\r\n        {header.Key}: {JsonConvert.SerializeObject(header.Value)}";
+                }
+                info += "\r\n    Content";
+                foreach (var input in _input)
+                {
+                    if (input.Value == null) continue;
+                    info += $"\r\n        {input.Key}: {input.Value}";
+                }
+
+                ConsoleHelper.WriteLine(info, ConsoleHelper.EMode.message);
+
+                return Json(_input);
+            }
+            catch (Exception ex)
+            {
+                ex = ExceptionHelper.GetReal(ex);
+                ConsoleHelper.Log($"{ex.Message}", ConsoleHelper.EMode.error);
+                Program.log.Error(ex);
+
+                return Content(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
--- WebApiServerSimulation/Program.cs
+++ WebApiServerSimulation/Program.cs
@@ -3,12 +3,18 @@ using Min_Helpers.LogHelper;
 using System;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
 
 namespace WebApiServerSimulation
 {
-    class Program
+    public class Program
     {
+        public static Log log { get; set; }
+
         static void Main(string[] args)
         {
             Thread.CurrentThread.CurrentCulture = new CultureInfo("en");
@@ -16,7 +22,7 @@ namespace WebApiServerSimulation
 
             ConsoleHelper.Initialize();
 
-            Log log = new Log();
+            log = new Log();
             log.Initialize(new FileInfo($"{AppDomain.CurrentDomain.BaseDirectory}log4net.config"));
 
             try
@@ -25,6 +31,36 @@ namespace WebApiServerSimulation
 
                 ConsoleHelper.Write("Listen Port: ", ConsoleHelper.EMode.question);
                 int port = Convert.ToInt32(Console.ReadLine());
+                log.Info($"Listen Port: {port}");   
+
+                HttpSelfHostConfiguration config = new HttpSelfHostConfiguration($"http://127.0.0.1:{port}");
+                //config.Routes.MapHttpRoute(
+                //    name: "Api",
+                //    routeTemplate: "{controller}/{action}/{id}",
+                //    defaults: new { id = RouteParameter.Optional }
+                //);
+
+                config.Routes.MapHttpRoute(
+                    name: "Default",
+                    routeTemplate: "{*url}",
+                    defaults: new { controller = "Index", action = "Handle" }
+                );
+
+                var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+                config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+                using (HttpSelfHostServer httpServer = new HttpSelfHostServer(config))
+                {
+                    httpServer.OpenAsync().Wait();
+
+                    while (true)
+                    {
+                        string line = Console.ReadLine();
+                        if (line == "q" || line == "quit" || line == "bye" || line == "exit") break;
+                    }
+
+                    httpServer.CloseAsync().Wait();
+                }
             }
             catch (Exception ex)
             {
